4652281ce821a7a1ea6bb0bdbdae27d7d95c58a6
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2018-01-25 13:46:07 -0200

    removing unecessary files

--- setup-eap-sso-openshift.sh
+++
@@ -1,3 +0,0 @@
-oc project sso
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/secrets/eap7-app-secret.json
-oc process eap70-sso-s2i --param-file=eap-sso-template.env -n openshift | oc create -n sso -f -
